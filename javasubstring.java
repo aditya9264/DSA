154e74fc9256fd9953ed5931c154d5fa4ed2f5a6
aditya9264 <aditya9919494435@gmail.com>
2023-06-20 16:52:54 +0530

    javasubstring

---
+++ javasubstring.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class javasubstring {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+
+        System.out.println(s.substring(start, end));
+
+    }
+}
