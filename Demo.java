e0884efd6c4ecb6a419a4fbe8ec8cf1238111c02
aditya9264 <aditya9919494435@gmail.com>
2024-04-23 13:20:24 +0530

    demo commit

---
+++ Demo.java
@@ -0,0 +1,8 @@
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello" +"world");
+        System.out.println(10+20+"hello");
+        System.out.println("hello"+10+20);
+    }
+
+}
